f46dea05368e957f3cbaf0a57ee98892214f387b
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-10-01 14:24:38 -0500

    Enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  kodiaksApi: "https://kodiaksdev.somee.com/api"
+  //kodiaksApi: "https://localhost:7226/api"
 };
 
 /*
