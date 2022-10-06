2d0ed441a9c5c9b09d1c8a2d95b5ff84b4f32c3d
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-10-05 23:30:40 -0500

    Diseño casi terminado

--- src/app/application/services/role.service.ts
+++ src/app/application/services/role.service.ts
@@ -12,6 +12,7 @@ export class RoleService {
   constructor(private httpCliente: HttpClient) { }
 
   GetRole(id?: number): Observable<any> {
+    //Llamada del servicio
     return this.httpCliente.get<any>(id != null ? this.urlRole + "?id=" + id : this.urlRole, {});
   }
 }
